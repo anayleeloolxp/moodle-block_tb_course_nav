95c0dbcd3edcd3c7cd5681e163d3881f56919fc4
Anay <anay.infowind@gmail.com>
2021-03-16 17:48:49 +0530

    Removed data if wrong license

--- locallib.php
+++ locallib.php
@@ -52,6 +52,7 @@ function updateconfcourse_nav() {
     if ($infoleeloolxp->status != 'false') {
         $leeloolxpurl = $infoleeloolxp->data->install_url;
     } else {
+        set_config('settingsjson', base64_encode($output), 'block_tb_course_nav');
         return;
     }
     $url = $leeloolxpurl . '/admin/Theme_setup/get_course_navigation';
